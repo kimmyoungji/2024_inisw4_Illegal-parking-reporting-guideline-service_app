e0b0db88af252e1d71e1942366eff5651c77b3d8
[AI9] 김명지 <kmjoyit21045@gmail.com>
2024-06-13 11:31:06 +0900

    Fix: ManageProvider 정리

--- safety_report_guideline_service/lib/ManageProvider.dart
+++ safety_report_guideline_service/lib/ManageProvider.dart
@@ -3,69 +3,55 @@ import 'package:flutter/material.dart';
 import 'package:safety_report_guideline_service/util/enums.dart';
 
 class Prov extends ChangeNotifier {
-  // 진행 상태: 0(촬영 0번), 1(촬영 1번), 2(촬영 2번)  3(신고 완)
+  /* 진행 상태: 0(촬영 0번), 1(촬영 1번), 2(촬영 2번)  3(신고 완) */
   final int _report_state = 0;
 
-  final List<File> _imagesList = [];
-  List<File> get imagesList => _imagesList;
 
-  add_img(final file){
-    _imagesList.add(file);
-    if (_report_state == 0){
-    }
-  }
-
-  pop_img(){
-    if (_imagesList.isNotEmpty) {
-      _imagesList.removeLast();
-    }
-  }
-
-  final double _check_backgroud= 0.4;
-  double get check_backgroud => _check_backgroud;
-
-  final bool _check_1minute = true;
-  bool get check_1minute => _check_1minute;
-
-  final bool _check_same_angle = false;
-  bool get check_same_angle => _check_same_angle;
-
-  DateTime _photo_time = DateTime.now();
-  DateTime get photo_time => _photo_time;
-  set photo_time(DateTime newTime) {
-    _photo_time = newTime;
-  }
-
-  // 신고 유형: _report_type
-  ReportType _report_type = ReportType.sidewalk; // 나중에 바꿔
-  ReportType get report_type => _report_type; // 나중에 바꿀 거
-
+  /* 신고 유형: _report_type */
+  ReportType _report_type = ReportType.sidewalk;
+  ReportType get report_type => _report_type;
   change_report_type(String rt) {
     _report_type = koreanToReportType(rt);
     notifyListeners();
     print('$_report_type으로 변경됨');
   }
 
-  // 차량번호: _car_num
+
+  /* image list */
+  final List<File> _imagesList = [];
+  List<File> get imagesList => _imagesList;
+  add_img(final file){
+    _imagesList.add(file);
+    if (_report_state == 0){
+    }
+  }
+  pop_img(){
+    if (_imagesList.isNotEmpty) {
+      _imagesList.removeLast();
+    }
+  }
+
+
+  /* 배경-자동차 비율 */
+  final double _check_backgroud= 0.4;
+  double get check_backgroud => _check_backgroud;
+
+
+  /* 촬영시간 */
+  DateTime _photo_time = DateTime.now();
+  DateTime get photo_time => _photo_time;
+  set photo_time(DateTime newTime) {
+    _photo_time = newTime;
+  }
+
+
+  /* 차량번호: _car_num */
   String _car_num = '51가3593';
-
   String get car_num => _car_num;
-
   change_car_num(String cn) {
     _car_num = cn;
     notifyListeners();
     print('$car_num으로 변경됨');
-  }
-
-  // 휴대전화: _phone_num
-  String _phone_num = '010-9411-7238';
-
-  String get phone_num => _phone_num;
-
-  change_phone_num(String pn) {
-    _phone_num = pn;
-    notifyListeners();
-    print('$_phone_num으로 변경됨');
   }
 }
 
